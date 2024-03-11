0364d71600f73c5b5ed353ffbd9f1ac67f33ae1e
zyte09 <allen.aranez32@gmail.com>
2024-03-11 10:36:10 +0800

    Trying if else

--- StudentFinancialAidSystemIfElse.java
+++ StudentFinancialAidSystemIfElse.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class StudentFinancialAidSystem {
+public class StudentFinancialAidSystemIfElse {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
 
